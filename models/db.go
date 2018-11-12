b81c7a7c21bcd783d617b0b006b5cfb48777336a
Jack Chu <chuchuanbao@gmail.com>
2018-11-12 13:53:54 +0800

    fix(存入的时间): 解决存入数据库的时间为UTC时间，没有本地化的BUG

--- models/db.go
+++ models/db.go
@@ -16,7 +16,7 @@ func RegisterDB() {
 	db_schema := beego.AppConfig.String("db_schema")
 	db_user := beego.AppConfig.String("db_user")
 	db_passwd := beego.AppConfig.String("db_passwd")
-	jdbcUrl := db_user + ":" + db_passwd + "@tcp(" + db_host + ":" + db_port + ")/" + db_schema + "?charset=utf8"
+	jdbcUrl := db_user + ":" + db_passwd + "@tcp(" + db_host + ":" + db_port + ")/" + db_schema + "?charset=utf8&loc=Local"
 	beego.Info(fmt.Sprintf("connect to mysql server %v successfully !", db_host))
 	orm.RegisterDriver(_DB_Driver, orm.DRMySQL)
 	orm.RegisterDataBase("default", _DB_Driver, jdbcUrl, 30)
